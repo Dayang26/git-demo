80781c6248a895f66c9298a621e7f4ab1560b579
Dayang26 <hsyy26@qq.com>
2020-12-07 20:34:54 +0800

    12312

--- mybatis-8/src/main/java/com/snow/dao/BlogMapper.java
+++ mybatis-8/src/main/java/com/snow/dao/BlogMapper.java
@@ -2,11 +2,27 @@ package com.snow.dao;
 
 import com.snow.pojo.Blog;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Snow
  * @create 2020-12-03 16:54
  */
 public interface BlogMapper {
 
+    /**
+     * 向数据库中添加信息
+     *
+     * @param blog
+     * @return
+     */
     int addBook(Blog blog);
+
+    List<Blog> queryBlogWhere(Map map);
+
+    List<Blog> queryBlogChoose(Map map);
+
+    int updateBlog(Map map);
+
 }
--- mybatis-8/src/test/java/com/snow/dao/BlogTest.java
+++ mybatis-8/src/test/java/com/snow/dao/BlogTest.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Snow
@@ -22,12 +24,15 @@ public class BlogTest {
 
         blog.setId(IdUtils.getId());
         blog.setTitle("mybatis so easy");
+        blog.setAuthor("Snow");
         blog.setCreateTime(new Date());
         blog.setViews(9999);
         mapper.addBook(blog);
 
         blog.setId(IdUtils.getId());
         blog.setTitle("Jave so easy");
+        blog.setAuthor("Snow");
+
         blog.setCreateTime(new Date());
         blog.setViews(9999);
         mapper.addBook(blog);
@@ -35,6 +40,8 @@ public class BlogTest {
         blog.setId(IdUtils.getId());
         blog.setTitle("Springboot so easy");
         blog.setCreateTime(new Date());
+        blog.setAuthor("Snow");
+
         blog.setViews(9999);
         mapper.addBook(blog);
 
@@ -42,8 +49,68 @@ public class BlogTest {
         blog.setTitle("MicroService so easy");
         blog.setCreateTime(new Date());
         blog.setViews(9999);
+        blog.setAuthor("Snow");
+
         mapper.addBook(blog);
 
         sqlSession.close();
+    }
+
+    @Test
+    public void queryBlog() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+
+        HashMap map = new HashMap();
+
+        map.put("title", "mybatis so easy");
+        map.put("author", "Snow");
+
+        List<Blog> blogs = mapper.queryBlogWhere(map);
+
+        for (Blog blog : blogs) {
+            System.out.println(blog);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void queryBlogChoose() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+
+        HashMap map = new HashMap();
+
+
+        map.put("title", "mybatis so easy");
+        map.put("author", "Snow");
+        map.put("views", "9999");
+        List<Blog> blogs = mapper.queryBlogChoose(map);
+
+        for (Blog blog : blogs) {
+            System.out.println(blog);
+        }
+
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void updateBlog() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+
+        HashMap map = new HashMap();
+
+
+        map.put("title", "~~~~~~~~~~~~~~");
+        map.put("author", "Snow");
+        map.put("id", "dcde151c.e792.47a1.b1e1.d607f5d4ee61");
+        mapper.updateBlog(map);
+
+
+        sqlSession.close();
+
     }
 }
